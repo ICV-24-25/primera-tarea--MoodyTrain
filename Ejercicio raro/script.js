bc65bf9c360e2b5391148b4ce223601924943068
moodytrain <marcelotomasnm@gmail.com>
2025-03-19 10:59:44 +0100

    temperaturas

---
+++ Ejercicio raro/script.js
@@ -0,0 +1,7 @@
+const temperaturas = [
+    [23,25,22,16,24,22,27],
+    [33,22,26,19,26,24,25],
+    [24,27,21,13,30,26,17],
+    [23,25,22,16,24,22,27]];
+
+const localidades = ["Vera","Huercal-Overa","Albox","Mojacar"];
